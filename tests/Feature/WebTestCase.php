176acf3792fc645adbf2d5a910381a372d562436
ElisDN <mail@elisdn.ru>
2018-10-05 13:38:59 +0300

    Added feature tests fixtures

--- tests/Feature/WebTestCase.php
+++ tests/Feature/WebTestCase.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 namespace Test\Feature;
 
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\Stream;
 use Zend\Diactoros\Uri;
 use Zend\Expressive\Application;
 use Zend\Expressive\MiddlewareFactory;
 
 class WebTestCase extends TestCase
 {
+    private $fixtures = [];
+
     protected function get(string $uri): ResponseInterface
     {
         return $this->method($uri, 'GET');
@@ -35,14 +43,44 @@ class WebTestCase extends TestCase
         return $response;
     }
 
+    protected function loadFixtures(array $fixtures): void
+    {
+        $container = $this->container();
+        $em = $container->get(EntityManagerInterface::class);
+        $loader = new Loader();
+        foreach ($fixtures as $name => $class) {
+            if ($container->has($class)) {
+                $fixture = $container->get($class);
+            } else {
+                $fixture = new $class;
+            }
+            $loader->addFixture($fixture);
+            $this->fixtures[$name] = $fixture;
+        }
+        $executor = new ORMExecutor($em, new ORMPurger($em));
+        $executor->execute($loader->getFixtures());
+    }
+
+    protected function getFixture($name)
+    {
+        if (!array_key_exists($name, $this->fixtures)) {
+            throw new \InvalidArgumentException('Undefined fixture ' . $name);
+        }
+        return $this->fixtures[$name];
+    }
+
     private function app(): Application
     {
-        /** @var \Psr\Container\ContainerInterface $container */
-        $container = require 'config/container.php';
+        $container = $this->container();
         $app = $container->get(Application::class);
         $factory = $container->get(MiddlewareFactory::class);
         (require 'config/pipeline.php')($app, $factory, $container);
         (require 'config/routes.php')($app, $factory, $container);
         return $app;
+    }
+
+    private function container(): ContainerInterface
+    {
+        return require 'config/container.php';
     }
 }
